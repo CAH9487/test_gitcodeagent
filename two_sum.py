51a4e988357dc9c2f562be8b35d8767a53534324
CAH9487 <61491454+CAH9487@users.noreply.github.com>
2025-06-04 11:54:19 +0800

    style: add blank lines for pep8

--- two_sum.py
+++ two_sum.py
@@ -1,7 +1,8 @@
 from typing import List
 
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
         num_to_index = {}
         for i, num in enumerate(nums):
             complement = target - num
@@ -9,12 +10,14 @@ class Solution:
                 return [num_to_index[complement], i]
             num_to_index[num] = i
 
+
 def test_two_sum():
     s = Solution()
-    assert s.twoSum([2,7,11,15], 9) == [0,1] or s.twoSum([2,7,11,15], 9) == [1,0]
-    assert s.twoSum([3,2,4], 6) == [1,2] or s.twoSum([3,2,4], 6) == [2,1]
-    assert s.twoSum([3,3], 6) == [0,1] or s.twoSum([3,3], 6) == [1,0]
+    assert s.two_sum([2,7,11,15], 9) == [0,1] or s.two_sum([2,7,11,15], 9) == [1,0]
+    assert s.two_sum([3,2,4], 6) == [1,2] or s.two_sum([3,2,4], 6) == [2,1]
+    assert s.two_sum([3,3], 6) == [0,1] or s.two_sum([3,3], 6) == [1,0]
     print('All tests passed!')
+
 
 if __name__ == "__main__":
     test_two_sum()
